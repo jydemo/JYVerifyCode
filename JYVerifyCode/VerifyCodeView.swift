4bb1ef5e5c409ab59dcf2bd8930235c74fe0b16a
jydemo <jydemo@qq.com>
2017-02-27 10:59:18 +0800

    生成验证码

---
+++ JYVerifyCode/VerifyCodeView.swift
@@ -0,0 +1,15 @@
+//
+//  VerifyCodeView.swift
+//  JYVerifyCode
+//
+//  Created by atom on 2017/2/9.
+//  Copyright © 2017年 atom. All rights reserved.
+//
+
+import UIKit
+
+class VerifyCodeView: UIView {
+
+    
+
+}
